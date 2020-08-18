d96b2185fe7974f735d19a671806601c0fe89be1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-18 08:45:10 +0000

    Deploy dev.elhamnaseri.com to github.com/ehsangazar/elhamnaseri.git:gh-pages

---
+++ precache-manifest.504d8f9d3712f4c5454fb8657dfcd906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66126f4f9dbcace4d32edf7bd0a8cb89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d96cf1933e37aaad2af",
+    "url": "/static/css/main.10f77e75.chunk.css"
+  },
+  {
+    "revision": "4db34635a1a038fd67a6",
+    "url": "/static/js/2.55e6a6bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55e6a6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d96cf1933e37aaad2af",
+    "url": "/static/js/main.b5771dfb.chunk.js"
+  },
+  {
+    "revision": "f6289c954c3cf7a782f5",
+    "url": "/static/js/runtime-main.1c0f52b7.js"
+  },
+  {
+    "revision": "2144f880a2e46f46d1d790701b01ab01",
+    "url": "/static/media/elham.2144f880.png"
+  }
+]);
